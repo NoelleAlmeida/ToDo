9e02cb3d0f907f61ba4cbb9b11e57788c8c16078
NoelleAlmeida <noellealmeidasantos@outlook.com>
2026-03-02 19:47:04 -0300

    fix error

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
@@ -41,5 +42,9 @@ function AddTask({ onAddTaskSubmit }) {
     </div>
   );
 }
+
+AddTask.propTypes = {
+  onAddTaskSubmit: PropTypes.func.isRequired,
+};
 
 export default AddTask;
